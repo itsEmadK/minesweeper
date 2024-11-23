a6824af1a4e16305836d2d5c5bc2aee5b33f76ed
Emad <67466634+itsEmadK@users.noreply.github.com>
2024-11-23 23:06:51 +0330

    Add function to generate the minefield

--- script.js
+++ script.js
@@ -37,3 +37,58 @@ function reshape(arr, rows, columns) {
     return new2DArray;
 }
 
+function generateMinefield(rows, columns, firstTouchI, firstTouchJ, bombCount) {
+    let minefield = [];
+    for (let i = 0; i < rows; i++) {
+        const tempArray = [];
+        for (let j = 0; j < columns; j++) {
+            tempArray.push(false);
+        }
+        minefield.push(tempArray.slice());
+        tempArray.length = [];
+    }
+
+    minefield[firstTouchI][firstTouchJ] = null;
+    if (firstTouchJ > 0) {
+        minefield[firstTouchI][firstTouchJ - 1] = null;
+    }
+    if (firstTouchJ < columns - 1) {
+        minefield[firstTouchI][firstTouchJ + 1] = null;
+    }
+    if (firstTouchI > 0) {
+        minefield[firstTouchI - 1][firstTouchJ] = null;
+        if (firstTouchJ > 0) {
+            minefield[firstTouchI - 1][firstTouchJ - 1] = null;
+        }
+        if (firstTouchJ < columns - 1) {
+            minefield[firstTouchI - 1][firstTouchJ + 1] = null;
+        }
+    }
+    if (firstTouchI < columns - 1) {
+        minefield[firstTouchI + 1][firstTouchJ] = null;
+        if (firstTouchJ > 0) {
+            minefield[firstTouchI + 1][firstTouchJ - 1] = null;
+        }
+        if (firstTouchJ < columns - 1) {
+            minefield[firstTouchI + 1][firstTouchJ + 1] = null;
+        }
+    }
+
+    const flatMineField = minefield.flat();
+
+    let k = 0;
+    while (k < bombCount) {
+        if (flatMineField[k] !== null) {
+            flatMineField[k] = true;
+            k++;
+        }
+    }
+
+    shuffleInPlace(flatMineField, (item) => item === null);
+    minefield = reshape(flatMineField, rows, columns);
+    return minefield;
+
+}
+
+
+console.table(generateMinefield(5, 10, 3, 2, 20));
