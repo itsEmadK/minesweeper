9c03caaf5ee6b020c40a4dfad92493249674e9e0
Emad <67466634+itsEmadK@users.noreply.github.com>
2024-11-23 18:13:55 +0330

    Add function to return the shuffle of an array

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ function shuffleInPlace(arr) {
     }
     return arr;
 }
+
+function shuffle(arr) {
+    arr = arr.slice();
+    const n = arr.length;
+    for (let i = n - 1; i >= 1; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    return arr;
+}
