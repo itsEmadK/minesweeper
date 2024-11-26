76d9053f94d155bbabb10a13875f7d3c8a338cfb
Emad <67466634+itsEmadK@users.noreply.github.com>
2024-11-26 21:21:11 +0330

    Rename initMinefield func

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const minefield = [];
 let [minefieldRows, minefieldColumns] = [8, 12];
 let remainingBombs = 18;
-initMinefield();
+initEmptyMinefield();
 
 const startingPoint = new Point(2, 3);
 minefield[startingPoint.i][startingPoint.j].isRevealed = true; //Reveal the starting point.
@@ -24,7 +24,7 @@ function Point(i, j) {
     this.i = i;
 }
 
-function initMinefield() {
+function initEmptyMinefield() {
     for (let i = 0; i < minefieldRows; i++) {
         const temp = [];
         for (let j = 0; j < minefieldColumns; j++) {
