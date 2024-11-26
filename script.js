efafbae90301cc6bb354861c6eba4d17951e7ae6
Emad <67466634+itsEmadK@users.noreply.github.com>
2024-11-26 20:51:40 +0330

    Rename x and y to j and i

--- script.js
+++ script.js
@@ -4,33 +4,33 @@ initMinefield();
 
 const startingPoint = new Point(2, 3);
 
-const x = startingPoint.x;
-const y = startingPoint.y;
-minefield[y][x].isRevealed = true; //Reveal the starting point.
+const j = startingPoint.j;
+const i = startingPoint.i;
+minefield[i][j].isRevealed = true; //Reveal the starting point.
 
 //Reveal all the adjacent cells of the starting point:
-if (x > 0) {
-    minefield[y][x - 1].isRevealed = true;
+if (j > 0) {
+    minefield[i][j - 1].isRevealed = true;
 }
-if (x < minefieldColumns) {
-    minefield[y][x + 1].isRevealed = true;
+if (j < minefieldColumns) {
+    minefield[i][j + 1].isRevealed = true;
 }
-if (y > 0) {
-    minefield[y - 1][x].isRevealed = true;
-    if (x > 0) {
-        minefield[y - 1][x - 1].isRevealed = true;
+if (i > 0) {
+    minefield[i - 1][j].isRevealed = true;
+    if (j > 0) {
+        minefield[i - 1][j - 1].isRevealed = true;
     }
-    if (x < minefieldColumns) {
-        minefield[y - 1][x + 1].isRevealed = true;
+    if (j < minefieldColumns) {
+        minefield[i - 1][j + 1].isRevealed = true;
     }
 }
-if (y < minefieldRows) {
-    minefield[y + 1][x].isRevealed = true;
-    if (x > 0) {
-        minefield[y + 1][x - 1].isRevealed = true;
+if (i < minefieldRows) {
+    minefield[i + 1][j].isRevealed = true;
+    if (j > 0) {
+        minefield[i + 1][j - 1].isRevealed = true;
     }
-    if (x < minefieldColumns) {
-        minefield[y + 1][x + 1].isRevealed = true;
+    if (j < minefieldColumns) {
+        minefield[i + 1][j + 1].isRevealed = true;
     }
 }
 
@@ -53,7 +53,7 @@ function Cell() {
     this.isRevealed = false;
 }
 
-function Point(x, y) {
-    this.x = x;
-    this.y = y;
+function Point(i, j) {
+    this.j = j;
+    this.i = i;
 }
