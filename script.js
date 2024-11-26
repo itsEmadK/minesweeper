2a5e8d136539fb7f093ba3884b5ccfff32aa4fbf
Emad <67466634+itsEmadK@users.noreply.github.com>
2024-11-26 20:36:26 +0330

    Move initializing the empty  minefield into a new function

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 const minefield = [];
 let [rows, columns] = [8, 12];
-for (let i = 0; i < rows; i++) {
-    const temp = [];
-    for (let j = 0; j < columns; j++) {
-        temp.push(new Cell());
+initMinefield();
+
+function initMinefield() {
+    for (let i = 0; i < rows; i++) {
+        const temp = [];
+        for (let j = 0; j < columns; j++) {
+            temp.push(new Cell());
+        }
+        minefield.push(temp.slice());
+        temp.length = 0;
     }
-    minefield.push(temp.slice());
-    temp.length = 0;
 }
-
-
 
 function Cell() {
     this.isBomb = false;
